67044df1c3950d72d840460e67a10423e71d46d7
Pritesh922004 <priteshprjapati6@gmail.com>
2025-10-06 10:00:30 +0530

    Insert Done

--- Client/Services/Insert.php
+++ Client/Services/Insert.php
@@ -1,25 +1,36 @@
-<?php 
+<?php
+$result;$Data;
 require 'db/db.php';
-
-if (isset($_POST['submit'])) {
-$Data = array(
-	'fullname' => $_POST['fullname'],
-	'companyName' => $_POST['companyName'],
-	'Email' => $_POST['Email'],
-	'phone' => $_POST['phone'],
-	'shipmentType' => $_POST['shipmentType'],
-	'transportMode' => $_POST['transportMode'],
-	'originCountry' => $_POST['originCountry'],
-	'originPort' => $_POST['originPort'],
-	'destCountry' => $_POST['destCountry'],
-	'destPort' => $_POST['destPort'],
-	'productDescription' => $_POST['productDescription'],
-	'hsCode' => $_POST['hsCode'],
-	'weight' => $_POST['weight'],
-	'quantity' => $_POST['quantity'],
-	'dimensions' => $_POST['dimensions'],
-	'instructions' => $_POST['instructions']
-);
+require 'Sql/Insert_Query.php';
+global $conn;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$Data = [
+		'fullname' => $_POST['fullName'],
+		'companyName' => $_POST['companyName'],
+		'Email' => $_POST['email'],
+		'phone' => $_POST['phone'],
+		'shipmentType' => $_POST['shipmentType'],
+		'transportMode' => $_POST['transportMode'],
+		'originCountry' => $_POST['originCountry'],
+		'originPort' => $_POST['originPort'],
+		'destCountry' => $_POST['destCountry'],
+		'destPort' => $_POST['destPort'],
+		'productDescription' => $_POST['productDescription'],
+		'hsCode' => $_POST['hsCode'],
+		'weight' => $_POST['weight'],
+		'quantity' => $_POST['quantity'],
+		'dimensions' => $_POST['dimensions'],
+		'instructions' => $_POST['instructions']
+	];
 
 }
+$result = InsertRecord($conn, $Data);
+if ($result) {
+	header('Location:Service?status=success');
+} else {
+	// header('Location:Service?status=failed');
+	// exit();
+	echo $result;
+}
+mysqli_close($conn);
 ?>
--- Client/Sql/Insert_Query.php
+++ Client/Sql/Insert_Query.php
@@ -1,18 +1,17 @@
 <?php
-function InsertRecord($conn,$data)
+function InsertRecord($conn, $data)
 {
-$query ='INSERT INTO Shipment_Details VALUES("","'.$data['fullname'].'","'.$data['companyName'].'","'.$data['Email'].'","'.$data['phone'].'","'.$data['shipmentType'].'","'.$data['transportMode'].'","'.$data['originCountry'].'","'.$data['originPort'].'","'.$data['destCountry'].'","'.$data['destPort'].'","'.$data['productDescription'].'","'.$data['hsCode'].'","'.$data['weight'].'","'.$data['quantity'].'","'.$data['dimensions'].'","'.$data['instructions'].'")';
-
-try{
-    if(mysqli_query($conn,$query))
-    {
-        header("Location: /Darsh/Client/Services?Status=Success");
+    try {
+        if ($data) {
+            $query = 'INSERT INTO Shipment_Details VALUES("","' . $data['fullname'] . '","' . $data['companyName'] . '","' . $data['Email'] . '","' . $data['phone'] . '","' . $data['shipmentType'] . '","' . $data['transportMode'] . '","' . $data['originCountry'] . '","' . $data['originPort'] . '","' . $data['destCountry'] . '","' . $data['destPort'] . '","' . $data['productDescription'] . '","' . $data['hsCode'] . '","' . $data['weight'] . '","' . $data['quantity'] . '","' . $data['dimensions'] . '","' . $data['instructions'] . '")';
+            return mysqli_query($conn, $query);
+        }
+        echo 'Data is not set';
     }
-}
-catch(Exception $e)
-{
-echo $e;
-}
+    catch (Exception $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+
 
 }
 ?>
